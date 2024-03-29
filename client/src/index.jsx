d9cbcc8e636ca463a9fad45b839963b22e1b3423
Anton Konstantynov <batann1990@gmail.com>
2024-03-29 10:57:33 +0200

    second commit

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/styles.scss";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const mainEl = document.querySelector("#root");
 const root = ReactDOM.createRoot(mainEl);
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
